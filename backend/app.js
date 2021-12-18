bd6b1204ce8c044b00df5e6c17444bbac376f016
SharonJseg <sharon.j.segal@gmail.com>
2021-12-18 23:24:30 +0200

    update cors again

--- backend/app.js
+++ backend/app.js
@@ -20,13 +20,8 @@ app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const corsOpt = {
-  origin: 'https://api.sharon.students.nomoreparties.site',
-  allowedHeaders: ['Content-type', 'Authorization'],
-};
-
-app.use(cors(corsOpt));
-// app.options('*', cors());
+app.use(cors());
+app.options('*', cors());
 
 mongoose.connect('mongodb://localhost:27017/aroundb', {
   useNewUrlParser: true,
